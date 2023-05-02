78145ec7942d6657bc6a3853a69764248bba70f2
Dmitry <DOskolkov@bk.ru>
2023-05-02 17:55:44 +0300

    русский текст

---
+++ HW_2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int Random = new Random().Next(100,1000);
+Console.WriteLine(Random);
+
+int first = (Random / 10) % 10;
+
+Console.WriteLine($"{first}");
